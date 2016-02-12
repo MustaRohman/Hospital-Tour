cf16084929bc06cd3d2d9655e55cd15019e48bc2
Musta Rohman <mustarohman@gmail.com>
2016-02-12 22:44:40 +0000

    Changed layout and design of first screen. Added Tour Activity

--- Android Application/Prototype/app/src/main/java/com/example/mustarohman/prototype/MainActivity.java
+++ Android Application/Prototype/app/src/main/java/com/example/mustarohman/prototype/MainActivity.java
@@ -1,23 +1,39 @@
 package com.example.mustarohman.prototype;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TOUR_CODE =  "Tour Code";
+    private  ArrayList<String> tourCodes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tourCodes = new ArrayList<>();
+        tourCodes.add("BROMP100");
+        tourCodes.add("BROMP200");
+        tourCodes.add("BROMP300");
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setLogo(R.drawable.ic_lightbulb_outline_black_24dp);
-        toolbar.setTitle("Royal Brompton Hospital");
+        toolbar.setLogo(R.drawable.ic_lightbulb_outline_white_24dp);
+        toolbar.setTitle("Hive Tours");
         setSupportActionBar(toolbar);
 
     }
@@ -35,5 +51,22 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickStartBtn(View view) {
+        Intent intent = new Intent(this, TourActivity.class);
+        EditText codeEditText = (EditText) findViewById(R.id.code_edit);
+
+        if (tourCodes.contains(codeEditText.getText().toString())){
+            intent.putExtra(TOUR_CODE, codeEditText.getText());
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Incorrect tour code", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void onClickHelpBtn(View view) {
+        
     }
 }
