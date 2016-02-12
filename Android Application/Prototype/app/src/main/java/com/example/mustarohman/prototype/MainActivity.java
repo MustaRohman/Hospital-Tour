95d4649d3b4f296803902443a8f166e21da35147
Musta Rohman <mustarohman@gmail.com>
2016-02-12 20:46:12 +0000

    Started first prototype. Activity/layout created for initial screen where user enters tour code

---
+++ Android Application/Prototype/app/src/main/java/com/example/mustarohman/prototype/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.mustarohman.prototype;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.drawable.ic_lightbulb_outline_black_24dp);
+        toolbar.setTitle("Royal Brompton Hospital");
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
