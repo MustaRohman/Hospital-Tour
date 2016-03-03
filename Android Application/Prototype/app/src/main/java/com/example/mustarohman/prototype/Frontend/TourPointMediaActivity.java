d7d0f11b25c0077e1e8463674d7ac8b178c589bd
Musta Rohman <mustarohman@gmail.com>
2016-03-03 12:19:49 +0000

    changed layout of image media in TourPointMedia activity

--- Android Application/Prototype/app/src/main/java/com/example/mustarohman/prototype/Frontend/TourPointMediaActivity.java
+++ Android Application/Prototype/app/src/main/java/com/example/mustarohman/prototype/Frontend/TourPointMediaActivity.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.GridLayout;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.example.mustarohman.prototype.R;
@@ -22,7 +24,7 @@ public class TourPointMediaActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tour_point);
+        setContentView(R.layout.activity_tour_point_media);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Tour Point Info");
         setSupportActionBar(toolbar);
@@ -47,11 +49,12 @@ public class TourPointMediaActivity extends AppCompatActivity {
         LayoutInflater inflater = getLayoutInflater();
 
         for (int i = 0; i < 3; i++){
-            View imageView = inflater.inflate(R.layout.view_image_test, null);
-            ImageView img = (ImageView) imageView.findViewById(R.id.image_view);
-            img.setImageResource(R.drawable.cardiac5);
-
-            linearLayout.addView(imageView, i);
+            ImageButton imageView =  (ImageButton) inflater.inflate(R.layout.view_media_image_visible, null);
+            LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 300);
+            if (i == 1) imageView.setImageResource(R.drawable.hospital);
+            param.setMargins(20,20,20,20);
+            imageView.setLayoutParams(param);
+            linearLayout.addView(imageView);
             Log.d("TourPointMediaActivity", "view added");
         }
 
