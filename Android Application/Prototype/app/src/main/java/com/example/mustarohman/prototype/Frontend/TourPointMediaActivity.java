0f71a691b5c298ae93e68bad58fc1cc7ed201727
Musta Rohman <mustarohman@gmail.com>
2016-03-04 00:36:12 +0000

    Image on click animation now works. Image zooms out to show full image

--- Android Application/Prototype/app/src/main/java/com/example/mustarohman/prototype/Frontend/TourPointMediaActivity.java
+++ Android Application/Prototype/app/src/main/java/com/example/mustarohman/prototype/Frontend/TourPointMediaActivity.java
@@ -1,12 +1,18 @@
 package com.example.mustarohman.prototype.Frontend;
 
 import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -40,6 +46,9 @@ public class TourPointMediaActivity extends AppCompatActivity {
 
         mShortAnimationDuration = getResources().getInteger(
                 android.R.integer.config_shortAnimTime);
+
+
+
         //Which will changed
         imageFilePaths = new ArrayList<>();
 
@@ -57,7 +66,7 @@ public class TourPointMediaActivity extends AppCompatActivity {
 
             int imageResource = R.drawable.cardiac5;
             if (i == 1) imageResource = R.drawable.hospital;
-            setImageButtonAndAnimation(inflater, imageResource);
+            setImageButton(inflater, imageResource);
 
         }
 
@@ -68,26 +77,145 @@ public class TourPointMediaActivity extends AppCompatActivity {
      * @param inflater
      * @param res
      */
-    private void setImageButtonAndAnimation(LayoutInflater inflater, int res){
+    private void setImageButton(LayoutInflater inflater, final int res){
         View imageView =  inflater.inflate(R.layout.view_media_image_visible, null);
-        ImageButton imageButton = (ImageButton) imageView.findViewById(R.id.image_button);
+        final ImageButton imageButton = (ImageButton) imageView.findViewById(R.id.image_button);
         imageButton.setImageResource(res);
         LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 300);
         param.setMargins(20,20,20,20);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                zoomImage(imageButton, res);
+            }
+        });
 
-        ImageView enlargedImage = new ImageView(this);
-        enlargedImage.setImageResource(res);
-        FrameLayout.LayoutParams frameParam = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
-                FrameLayout.LayoutParams.MATCH_PARENT);
-        enlargedImage.setLayoutParams(frameParam);
-        enlargedImage.setVisibility(View.INVISIBLE);
-        enlargedImage.setContentDescription("Cardiac");
+//        ImageView enlargedImage = new ImageView(this);
+//        enlargedImage.setImageResource(res);
+//        FrameLayout.LayoutParams frameParam = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+//                FrameLayout.LayoutParams.MATCH_PARENT);
+//        enlargedImage.setLayoutParams(frameParam);
+//        enlargedImage.setVisibility(View.INVISIBLE);
+//        enlargedImage.setContentDescription("Cardiac");
 
         linearLayout.addView(imageView);
-        mainFrame.addView(enlargedImage);
+//        mainFrame.addView(enlargedImage);
         imageView.setLayoutParams(param);
         Log.d("TourPointMediaActivity", "view added");
 
+    }
+
+    private void zoomImage(final ImageButton imageButton, int res) {
+        if (mCurrentAnimator != null) {
+            mCurrentAnimator.cancel();
+        }
+
+        final ImageView expandedImageView = (ImageView) findViewById(
+                R.id.expanded_image);
+        expandedImageView.setImageResource(res);
+
+        final Rect startBounds = new Rect();
+        final Rect finalBounds = new Rect();
+        final Point globalOffset = new Point();
+
+        imageButton.getGlobalVisibleRect(startBounds);
+        findViewById(R.id.main_frame)
+                .getGlobalVisibleRect(finalBounds, globalOffset);
+        startBounds.offset(-globalOffset.x, -globalOffset.y);
+        finalBounds.offset(-globalOffset.x, -globalOffset.y);
+
+        float startScale;
+        if ((float) finalBounds.width() / finalBounds.height()
+                > (float) startBounds.width() / startBounds.height()) {
+            // Extend start bounds horizontally
+            startScale = (float) startBounds.height() / finalBounds.height();
+            float startWidth = startScale * finalBounds.width();
+            float deltaWidth = (startWidth - startBounds.width()) / 2;
+            startBounds.left -= deltaWidth;
+            startBounds.right += deltaWidth;
+        } else {
+            // Extend start bounds vertically
+            startScale = (float) startBounds.width() / finalBounds.width();
+            float startHeight = startScale * finalBounds.height();
+            float deltaHeight = (startHeight - startBounds.height()) / 2;
+            startBounds.top -= deltaHeight;
+            startBounds.bottom += deltaHeight;
+        }
+
+        imageButton.setAlpha(0f);
+        expandedImageView.setVisibility(View.VISIBLE);
+
+        expandedImageView.setPivotX(0f);
+        expandedImageView.setPivotY(0f);
+
+        AnimatorSet set = new AnimatorSet();
+        set
+                .play(ObjectAnimator.ofFloat(expandedImageView, View.X,
+                        startBounds.left, finalBounds.left))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y,
+                        startBounds.top, finalBounds.top))
+                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X,
+                        startScale, 1f)).with(ObjectAnimator.ofFloat(expandedImageView,
+                View.SCALE_Y, startScale, 1f));
+        set.setDuration(mShortAnimationDuration);
+        set.setInterpolator(new DecelerateInterpolator());
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mCurrentAnimator = null;
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                mCurrentAnimator = null;
+            }
+        });
+        set.start();
+        mCurrentAnimator = set;
+
+        final float startScaleFinal = startScale;
+        expandedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentAnimator != null) {
+                    mCurrentAnimator.cancel();
+                }
+
+                // Animate the four positioning/sizing properties in parallel,
+                // back to their original values.
+                AnimatorSet set = new AnimatorSet();
+                set.play(ObjectAnimator
+                        .ofFloat(expandedImageView, View.X, startBounds.left))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.Y,startBounds.top))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_X, startScaleFinal))
+                        .with(ObjectAnimator
+                                .ofFloat(expandedImageView,
+                                        View.SCALE_Y, startScaleFinal));
+                set.setDuration(mShortAnimationDuration);
+                set.setInterpolator(new DecelerateInterpolator());
+                set.addListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        imageButton.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+                        imageButton.setAlpha(1f);
+                        expandedImageView.setVisibility(View.GONE);
+                        mCurrentAnimator = null;
+                    }
+                });
+                set.start();
+                mCurrentAnimator = set;
+            }
+        });
     }
 
     private void addVideoViews(){
